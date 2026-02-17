d337c0e831502f4e1c11e33778e3b3172467a821
nogarx20 <75626088+nogarx20@users.noreply.github.com>
2026-02-17 12:17:45 -0500

    Update attendance.routes.ts

--- src/modules/attendance/attendance.routes.ts
+++ src/modules/attendance/attendance.routes.ts
@@ -1,4 +1,3 @@
-
 import { Router } from 'express';
 import { AttendanceController } from './attendance.controller';
 import { authMiddleware } from '../../middlewares/auth.middleware';
@@ -12,5 +11,6 @@ router.use(authMiddleware, tenantMiddleware);
 
 // Endpoint para realizar marcaje
 router.post('/mark', rbacMiddleware('attendance.create'), controller.mark);
+router.post('/mark-pin', rbacMiddleware('attendance.create'), controller.markWithPin);
 
 export default router;
