0bd636e6b25fd5babd11a3333c6803c4e789de4f
nogarx20 <75626088+nogarx20@users.noreply.github.com>
2026-02-17 17:22:32 -0500

    Update biometrics.routes.ts

--- src/modules/biometrics/biometrics.routes.ts
+++ src/modules/biometrics/biometrics.routes.ts
@@ -1,4 +1,3 @@
-
 import { Router } from 'express';
 import { BiometricController } from './biometrics.controller';
 import { authMiddleware } from '../../middlewares/auth.middleware';
@@ -10,15 +9,14 @@ const controller = new BiometricController();
 
 router.use(authMiddleware, tenantMiddleware);
 
-// FaceID
 router.post('/enroll', rbacMiddleware('collaborators.create'), controller.enroll);
 router.post('/verify-and-mark', rbacMiddleware('attendance.create'), controller.verifyAndMark);
 router.post('/identify-and-mark', rbacMiddleware('attendance.create'), controller.identifyAndMark);
 router.delete('/:collaboratorId', rbacMiddleware('collaborators.create'), controller.delete);
 
 // Huellas Dactilares
-router.post('/fingerprint/enroll', rbacMiddleware('collaborators.create'), controller.enrollFinger);
-router.get('/fingerprint/:collaboratorId', rbacMiddleware('collaborators.read'), controller.getFingers);
-router.delete('/fingerprint/:id', rbacMiddleware('collaborators.create'), controller.deleteFinger);
+router.get('/:collaboratorId/fingers', rbacMiddleware('collaborators.read'), controller.getFingers);
+router.post('/fingers', rbacMiddleware('collaborators.create'), controller.enrollFinger);
+router.delete('/fingers/:id', rbacMiddleware('collaborators.create'), controller.deleteFinger);
 
 export default router;
