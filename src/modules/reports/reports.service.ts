3ad1b6ed9860e5f14163193957414120a47269a2
Norman Galvis Ramirez <75626088+nogarx20@users.noreply.github.com>
2026-05-25 09:53:13 -0500

    Update reports.service.ts

--- src/modules/reports/reports.service.ts
+++ src/modules/reports/reports.service.ts
@@ -303,14 +303,14 @@ export class ReportsService {
           },
           shift: {
             name: row.shift_name,
-            shift_prefix: row.shift_prefix || row.prefix || '?', // Aseguramos que tome el prefijo disponible
+            shift_prefix: row.shift_prefix || '?',
             type: row.shift_type || 'N/A',
             start_time: row.start_time,
             end_time: row.end_time,
             start_time_2: row.start_time_2,
             end_time_2: row.end_time_2,
-            lunch_start: row.lunch_start || null,
-            lunch_end: row.lunch_end || null
+            lunch_start: row.lunch_start,
+            lunch_end: row.lunch_end
           },
           cost_center: row.cost_center_name,
           markings: []
